713420c1b5add72394de5c44bf608b8c375362ab
Fromboy <93315528+Fromboy@users.noreply.github.com>
2022-12-16 15:22:38 +0800

    Update kuwo.js

--- kuwo.js
+++ kuwo.js
@@ -13,8 +13,8 @@
 
 
 [rewrite_local]
-^https?:\/\/.*(kuwo|lrts).*(music\.pay\?ne|\/a\.p|v\d\/api\/(user\/in|pay\/vi)|\w{3}\/v\d\/user\/vi|\w{3}\/spi\/ms|\w{3}\/v\d\/the|\w{3}\/enc\/use) url script-response-body https://raw.githubusercontent.com/gjwj666/qx/main/kw.js
-^https?:\/\/.*(kuwo|lrts).*(music\.pay\?ui) url script-request-header https://raw.githubusercontent.com/gjwj666/qx/main/kw.js
+^https?:\/\/.*(kuwo|lrts).*(music\.pay\?ne|\/a\.p|v\d\/api\/(user\/in|pay\/vi)|\w{3}\/v\d\/user\/vi|\w{3}\/spi\/ms|\w{3}\/v\d\/the|\w{3}\/enc\/use) url script-response-body https://raw.githubusercontent.com/Fromboy/JavaScript/Scripts/kuwo.js
+^https?:\/\/.*(kuwo|lrts).*(music\.pay\?ui) url script-request-header https://raw.githubusercontent.com/Fromboy/JavaScript/Scripts/kuwo.js
 
 [mitm] 
 
