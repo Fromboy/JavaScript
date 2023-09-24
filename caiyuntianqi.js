867e964aa4f64df2333a98d8f230267779a0fb07
Fromboy <93315528+Fromboy@users.noreply.github.com>
2023-09-24 21:02:29 +0800

    Update caiyuntianqi.js

--- caiyuntianqi.js
+++ caiyuntianqi.js
@@ -1,17 +1,19 @@
-  #!name=彩云天气PRO
-  #!desc=解锁会员
-  # 更新日期:2023-09-24 20:58:58
-  #  Quantumult X
-  #  [rewrite_local]
-  #  普通版广告
-  # ^https:\/\/ad\.cyapi\.cn\/v2\/req\?app_name=weather url reject-dict
-  #   重写
-  #  ^https:\/\/biz\.cyapi\.cn\/v2\/user url script-response-body https://raw.githubusercontent.com/Fromboy/JavaScript/Scripts/caiyuntianqi.js
-  #  ^https:\/\/wrapper\.cyapi\.cn\/v1\/(satellite|nafp\/origin_images) url script-request-header https://raw.githubusercontent.com/Fromboy/JavaScript/Scripts/caiyuntianqi.js
-  #  [MITM]
-  #  hostname = *.cyapi.cn
-  
-  var huihui = {},
+/*
+彩云天气
+更新2023-09-25
+====================================
+[rewrite_local]
+# 普通版广告
+^https:\/\/ad\.cyapi\.cn\/v2\/req\?app_name=weather url reject-dict
+
+^https:\/\/biz\.cyapi\.cn\/v2\/user url script-response-body https://raw.githubusercontent.com/wf021325/qx/master/js/caiyun.js
+^https:\/\/wrapper\.cyapi\.cn\/v1\/(satellite|nafp\/origin_images) url script-request-header https://raw.githubusercontent.com/wf021325/qx/master/js/caiyun.js
+
+[mitm]
+hostname = *.cyapi.cn
+====================================
+ */
+var huihui = {},
     url = $request.url;
 if (url.includes("/v2/user")) {
     let obj = JSON.parse($response.body);
