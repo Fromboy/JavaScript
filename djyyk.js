a1d6d8cb2b7987e9326a0f392e1a2e1f167327fb
Fromboy <93315528+Fromboy@users.noreply.github.com>
2025-08-12 20:54:32 +0800

    Update djyyk.js

--- djyyk.js
+++ djyyk.js
@@ -5,14 +5,14 @@
 软件版本：2.6.7
 下载地址：
 脚本作者：
-更新时间：2025年
-电报频道：https://t.me/GieGie777
+更新时间：2025年8月12
+电报频道：
 问题反馈：
 使用声明：此脚本仅供学习与交流，请在下载使用24小时内删除！请勿在中国大陆转载与贩卖！
 *******************************
 [rewrite_local]
 # >dj音乐库-dj电音音乐播放器 VIP
-^http:\/\/app-i.dj-5.com\/api\/User\/Info url script-response-body https://raw.githubusercontent.com/WeiGiegie/666/main/djyyk.js
+^http:\/\/app-i.dj-5.com\/api\/User\/Info url script-response-body https://raw.githubusercontent.com/Fromboy/JavaScript/refs/heads/Scripts/djyyk.js
 
 [mitm]
 hostname = app-i.dj-5.com
