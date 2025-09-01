146739876c63f8a3319b1ff7deab70a2b2013a8a
Fromboy <93315528+Fromboy@users.noreply.github.com>
2025-09-01 17:11:17 +0800

    Update and rename Rename Node.js to RenameNode.js

--- RenameNode.js
+++ RenameNode.js
@@ -1,12 +1,9 @@
 /*
 // Quantumult X 节点名称和国旗替换脚本 (支持常见国家)
 // 该脚本将根据节点名称中的关键词匹配替换相应的国家名称及国旗。
-
+// 把该脚本写入重写并打开解析器
 [rewrite_local]
 ^https?:\/\/.*\/path\/to\/nodeRename$ url script-response-body https://raw.githubusercontent.com/Fromboy/JavaScript/Scripts/Rename%20Node.js
-
-
-
 
 */
 
