1c15e5b3b8395bf416b8cd20ab896e4824ad1e90
Fromboy <93315528+Fromboy@users.noreply.github.com>
2025-09-05 20:55:15 +0800

    Update MGYY2.js

--- MGYY2.js
+++ MGYY2.js
@@ -7,7 +7,7 @@
 # ^https://app\.c.nf\.migu\.cn/payment/watch-ad url reject-200
 # > 咪咕音乐vip、音质
 # ^https?:\/\/(u|c|app).(musicapp|(c|u).nf).migu.cn.+(user\/api|column\/startup|resource\/skin) url script-response-body https://raw.githubusercontent.com/Fromboy/JavaScript/refs/heads/Scripts/MGYY2.js
-# ^https?:\/\/app.(c|pd).nf.migu.cn\/.*\/(listen-url|music\/batchQueryMusicPolicy|download-url).*$ url script-request-header https://raw.githubusercontent.com/WeiGiegie/666/main/MGYY2CK.js
+# ^https?:\/\/app.(c|pd).nf.migu.cn\/.*\/(listen-url|music\/batchQueryMusicPolicy|download-url).*$ url script-request-header https://raw.githubusercontent.com/Fromboy/JavaScript/refs/heads/Scripts/MGYY2CK.js
 # [mitm] 
 # hostname = *.migu.cn
 
