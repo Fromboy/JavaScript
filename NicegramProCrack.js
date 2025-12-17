3ef5936f0b8c18285c9f040f0bc9f43156679ae6
Fromboy <93315528+Fromboy@users.noreply.github.com>
2025-12-17 10:08:37 +0800

    Update NicegramProCrack.js for new URL handling

--- NicegramProCrack.js
+++ NicegramProCrack.js
@@ -9,7 +9,7 @@
 
 [rewrite_local]
   
-# > Nicegram☆解锁会员权限（2024-02-24）@ddgksf2013
+# > Nicegram☆解锁会员权限（2024-02-24）
 ^https?:\/\/nicegram\.cloud\/api\/v\d\/(ai-assistant\/purchase-list|user\/info|telegram\/auth) url script-response-body https://raw.githubusercontent.com/Fromboy/JavaScript/refs/heads/Scripts/NicegramProCrack.js
 
 [mitm] 
