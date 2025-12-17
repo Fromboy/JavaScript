20a666bf3657d881f9d04ed39552c6fc637d29e1
Fromboy <93315528+Fromboy@users.noreply.github.com>
2025-12-17 10:07:09 +0800

    Create NicegramProCrack.js

---
+++ NicegramProCrack.js
@@ -0,0 +1,23 @@
+/***********************************
+
+> 应用名称：Nicegram
+> 软件版本：1.5.6
+> 特别说明：⚠️⚠️⚠️
+          本脚本仅供学习交流使用，禁止转载售卖
+          ⚠️⚠️⚠️
+
+
+[rewrite_local]
+  
+# > Nicegram☆解锁会员权限（2024-02-24）@ddgksf2013
+^https?:\/\/nicegram\.cloud\/api\/v\d\/(ai-assistant\/purchase-list|user\/info|telegram\/auth) url script-response-body https://github.com/ddgksf2013/MoYu/raw/master/NicegramProCrack.js
+
+[mitm] 
+
+hostname=nicegram.cloud
+
+***********************************/
+
+
+var body=$response.body.replace(/subscription":\w+/g,'subscription":true');
+$done({body});
